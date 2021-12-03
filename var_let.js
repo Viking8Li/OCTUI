5fc86b66c41cb1edaaa4133210d3c6e451aae1bb
pamuba <partha.bora24@gmail.com>
2021-12-03 23:51:15 +0530

    1

---
+++ var_let.js
@@ -0,0 +1,15 @@
+// var funcs = [];
+
+// for(var i=0;i<3;i++){
+//     funcs[i] = function(){
+//         console.log("My Value:"+i)
+//     }
+// }
+
+// for(var j=0;j<3;j++){
+//     funcs[j]();
+// }
+
+function run(){
+    var a = 25;
+}
