cbb260a5174ca8c18bc7eaf0829624b0eacceaa5
pamuba <partha.bora24@gmail.com>
2021-12-14 00:43:35 +0530

    1411

--- Angular/12/routing.js
+++ Angular/12/routing.js
@@ -1,7 +1,20 @@
-var myApp = angular
-                  .module("app", []) //DI
-                  .controller("msg", function($scope){
+var myApp = angular.module("app", ['ngRoute']) //DI
+                
+myApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/first-msg',{
+            // template:'<b>This is the first message</b>'
+            templateUrl:'msg1.html',
+            controller:'message1'
+        })
+        .when('/second-msg',{
+            template:'<b>This is second message</b>'
+        })
+}])
 
-                  
-});
+myApp.controller('message1', ['$scope', function($scope){
+    $scope.a = 10;
+    $scope.b = 20;
+}])
+
 
