9df8af96e56fabd789a51662a1abe576742fdbc7
pamuba <partha.bora24@gmail.com>
2021-12-14 01:26:13 +0530

    454

--- Angular/12/routing.js
+++ Angular/12/routing.js
@@ -1,32 +1,61 @@
 var myApp = angular.module("app", ['ngRoute']) //DI
                 
 myApp.config(['$routeProvider', function($routeProvider){
+    // $routeProvider
+        // .when('/first-msg',{
+        //     // template:'<b>This is the first message</b>'
+        //     templateUrl:'msg1.html',
+        //     controller:'message1'
+        // })
+        // .when('/second-msg',{
+        //     template:'<b>This is second message</b>'
+        // })
+        // .when('/',{
+        //     template:'<b>Welcome to the App</b>'
+        // })
+        // .otherwise({
+        //     // template:'<b>Wrong Path</b>'
+        //     // redirectTo:"/"
+
+        //     redirectTo: function(){
+        //         alert("Wrong Path")
+        //         return "/"
+        //     }
+        // })
+
+
     $routeProvider
-        .when('/first-msg',{
-            // template:'<b>This is the first message</b>'
-            templateUrl:'msg1.html',
-            controller:'message1'
+        .when('/sumurl/:a/:b', {
+            templateUrl:'sumurl.html',
+            controller:'sumurl'
         })
-        .when('/second-msg',{
-            template:'<b>This is second message</b>'
+        .when('/suminput', {
+            templateUrl:'suminput.html',
+            controller:'suminput'
         })
         .when('/',{
-            template:'<b>Welcome to the App</b>'
-        })
-        .otherwise({
-            // template:'<b>Wrong Path</b>'
-            // redirectTo:"/"
-
-            redirectTo: function(){
-                alert("Wrong Path")
-                return "/"
-            }
+            template:'<b>Plz click a link on the left</b>'
         })
 }])
 
-myApp.controller('message1', ['$scope', function($scope){
-    $scope.a = 10;
-    $scope.b = 20;
-}])
+// myApp.controller('message1', ['$scope', function($scope){
+//     $scope.a = 10;
+//     $scope.b = 20;
+// }])
+
+
+myApp.controller('sumurl', function($scope, $routeParams){
+    $scope.a = $routeParams.a
+    $scope.b = $routeParams.b
+})
+myApp.controller('suminput', function($scope, $location, $interpolate){
+    $scope.a = 0
+    $scope.b = 0
+
+    $scope.doSum = function(){
+        var url = $interpolate('/sumurl/{{a}}/{{b}}')($scope)
+        $location.path(url)
+    }
+})
 
 
