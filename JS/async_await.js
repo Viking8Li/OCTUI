0122fac1f59700708e30879e30868172716f1393
pamuba <partha.bora24@gmail.com>
2021-12-16 00:12:54 +0530

    352

--- JS/async_await.js
+++ JS/async_await.js
@@ -10,3 +10,12 @@ function doubleAfter2Seconds(x){
 
 //this
 //super
+//IIFE
+// doubleAfter2Seconds(10).then(
+//     (value) => console.log(value)
+// )
+
+let sum = doubleAfter2Seconds(10).then(value=>value) + doubleAfter2Seconds(20).then(value=>value) 
++ doubleAfter2Seconds(30).then(value=>value)
+
+console.log(sum)
