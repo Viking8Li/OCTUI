17b9a13e7771ea9a9a7a06131812e10ba4215051
pamuba <partha.bora24@gmail.com>
2021-12-18 00:48:54 +0530

    393

--- NodeJS/18/files.js
+++ NodeJS/18/files.js
@@ -1,24 +1,43 @@
 var fs = require('fs')
 
+//sync
+// try{
+//     let data = fs.readFileSync('readMes.txt', 'utf-8');
+//     console.log(data)
+//     fs.writeFileSync('writeMe.txt', data)
+// }
+// catch(er){
+//     console.log('Error:',er.message)
+// }
 
-// let data = fs.readFileSync('readMe.txt', 'utf-8');
-// console.log(data)
 
-// fs.writeFileSync('writeMe.txt', data)
 
-fs.readFile('readMe.txt', 'utf-8', function(err, data){
-    if(err == null)
-        {
-            console.log(data); 
-            fs.writeFile('writeMe1.txt', data, function(err){
-                if(err != null){
-                    console.log(err.message)
-                }
-                else{
-                    console.log("File Written Successfully")
-                }
-            })
-        }
-    else
-        console.log(err.message)
-})
+//async doesnt work with try/catch
+//use callbacks
+// fs.readFile('readMe.txt', 'utf-8', function(err, data){
+//     if(err == null)
+//         {
+//             console.log(data); 
+//             fs.writeFile('writeMe1.txt', data, function(err){
+//                 if(err != null){
+//                     console.log(err.message)
+//                 }
+//                 else{
+//                     console.log("File Written Successfully")
+//                 }
+//             })
+//         }
+//     else
+//         console.log(err.message)
+// })
+
+
+//deleting a file
+// fs.unlink('writeMe.txt', (err)=>{
+//     if(err!=null){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log("File deleted successfully")
+//     }
+// })
