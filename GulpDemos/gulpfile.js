0c1066e93eedc562343567d7d6030b82dc87c017
pamuba <partha.bora24@gmail.com>
2022-01-04 08:51:47 +0530

    431

--- GulpDemos/gulpfile.js
+++ GulpDemos/gulpfile.js
@@ -1,5 +1,15 @@
 let gulp = require('gulp')
 let jshint = require('gulp-jshint')
+let cleanCSS = require('gulp-clean-css')
+let rename = require('gulp-rename')
+let sass = require('gulp-sass')
+let autoprefixer = require('gulp-autoprefixer')
+let sourcemaps = require('gulp-sourcemaps')
+const browserify = require('browserify')
+let babelify = require('babelify')
+let source = require('vinyl-source-stream')
+let buffer = require('vinyl-buffer')
+let uglify = require('gulp-uglify')
 
 
 //gulp --tasks lists all tasks
@@ -42,4 +52,104 @@ function task_jshint1(done){
 
 //export more than one task
 // exports.default = gulp.series(task_jshint, task_jshint1)
-exports.default = gulp.parallel(task_jshint, task_jshint1)
+// exports.default = gulp.parallel(task_jshint, task_jshint1)
+exports.both = gulp.parallel(task_jshint, task_jshint1)
+
+//gulp minify + rename css files
+// const styleSRC = './src/scss/style.css'
+// const styleDEST = './dist/css'
+
+// gulp.task('styles', function(done){
+//     gulp.src(styleSRC)
+//         .pipe(cleanCSS())
+//         .pipe(rename({suffix:'.min'}))
+//         .pipe(gulp.dest(styleDEST))
+//     done();
+// })
+
+///////////////////////////////////////////////////////////
+
+// gulp.task('minify-css', (done)=>{
+//     gulp.src(styleSRC)
+//         .pipe(cleanCSS({debug:true}, (details)=>{
+//             console.log(`${details.name}:${details.stats.originalSize}`);
+//             console.log(`${details.name}:${details.stats.minifiedSize}`);
+//         }))
+//         .pipe(rename({suffix:'.min'}))
+//         .pipe(gulp.dest((styleDEST)))
+//     done()
+// })
+
+/////////////////////////////////////////////////////////////////
+// sass compile + minify + rename
+// const styleSRC = './src/scss/style.scss'
+// const styleDEST = './dist/css'
+
+// gulp.task('styles', function(done){
+//     gulp.src(styleSRC)
+//         .pipe(sass({
+//             outputStyle:'compressed'
+//         }))
+//         .pipe(cleanCSS())
+//         .pipe(rename({suffix:'.min'}))
+//         .pipe(gulp.dest(styleDEST))
+//     done()
+// })
+
+////////////////////////////////////////////////////////////
+
+//sass compile + minify + rename + autoprefixer + sourcemaps
+const styleSRC = './src/scss/style.scss'
+const styleDEST = './dist/css'
+
+gulp.task('styles', function(done){
+    gulp.src(styleSRC)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle:'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            cascade:false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(styleDEST))
+    done()
+})
+//////////////////////////////////////////////////////
+//browserify
+//trnsform babelify
+//bundle
+//source
+//rename
+//bufffer
+//init sourcemaps
+//uglify
+//write sourcemaps
+//dist
+
+let jsSRC = 'script.js'
+let jsFolder = './src/js/'
+let jsDEST = './dist/js/'
+
+var jsFiles = [jsSRC]
+
+gulp.task('js', function(done){
+    jsFiles.map(function(entry){
+        return browserify({
+            entries:[jsFolder+entry]
+        })
+        .transform(babelify, {presets:['env']})
+        .bundle()
+        .pipe(source(entry))
+        .pipe(rename({extname:'.min.js'}))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps:true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(jsDEST))
+    });
+    done();
+})
