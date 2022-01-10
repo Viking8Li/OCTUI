296afa7ea32bfb33b933b9f603587a7c79a46953
pamuba <partha.bora24@gmail.com>
2022-01-11 01:31:01 +0530

    502

--- Jasmine/main.spec.js
+++ Jasmine/main.spec.js
@@ -1,19 +1,42 @@
 describe('Main', () => {
     describe('main.js', () => {
         describe('calculate()', ()=>{
-            xit('validates expression');
+            it('validates expression when the first number is passed', function(){
+                spyOn(window,'updateResult').and.stub();
+
+                calculate('a+3');
+                calculate('a+3');
+
+                expect(window.updateResult).toHaveBeenCalled();
+                expect(window.updateResult).toHaveBeenCalledWith('Expression not recognized')
+                expect(window.updateResult).toHaveBeenCalledTimes(2)
+            });
+            it('validates expression when the operator is invalid', function(){
+                spyOn(window,'updateResult').and.stub();
+
+                calculate('4_3');
+
+                expect(window.updateResult).toHaveBeenCalled();
+                expect(window.updateResult).toHaveBeenCalledWith('Expression not recognized')
+                expect(window.updateResult).toHaveBeenCalledTimes(1)
+            });
             xit('calls add');
             xit('calls subtract');
         })
         describe('updateResult()', ()=>{
-            it('adds resut to the DOM element', function(){
-                
-                const element = document.createElement('div');
+            let element;
+            //tear down function
+            beforeAll(function(){
+                element = document.createElement('div');
                 element.setAttribute('id', 'result');
                 document.body.appendChild(element);
-
+            })
+            afterAll(function(){
+                const element = document.getElementById('result')
+                document.body.removeChild(element);
+            })
+            it('adds resut to the DOM element', function(){
                 updateResult('100')
-
                 expect(element.innerText).toBe('100')
             })
         })
