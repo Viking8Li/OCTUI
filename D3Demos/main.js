2b640c328e8792160357ea4022dea567479725d6
pamuba <partha.bora24@gmail.com>
2022-01-14 01:06:13 +0530

    52

--- D3Demos/main.js
+++ D3Demos/main.js
@@ -1,3 +1,5 @@
+const MARGIN
+
 const svg = d3.select("#chart-area").append("svg")
                 .attr("width",400)
                 .attr("height",400)
@@ -7,11 +9,20 @@ d3.json("data/buildings.json").then(data => {
         d.height = Number(d.height)
     })
 
+//Band Scale(starting x, width)
+const x = d3.scaleBand()
+            .domain(data.map(d=>d.name))
+            .range([0, 400])
+            .paddingInner(0.3)
+            .paddingOuter(0.2)
+
+//test band Scale
+console.log(x("Lotte World Tower"))
+
 //Linear Scale
 const y = d3.scaleLinear()
-            .domain([0, 828])
+            .domain([0, d3.max(data, d => d.height)])
             .range([0,400])
-
 
 //cirtual rects created in a temp area of D3
 const rects = svg.selectAll("rect")
@@ -20,8 +31,8 @@ const rects = svg.selectAll("rect")
 //Enter selection(enter the rects into the DOM)
 rects.enter().append("rect")
             .attr("y",0)
-            .attr("x", (d,i) => i * 60)
-            .attr("width", 40)
+            .attr("x", (d,i) => x(d.name))
+            .attr("width", x.bandwidth)
             .attr("height", d => y(d.height))
             .attr("fill", "blue")
 
