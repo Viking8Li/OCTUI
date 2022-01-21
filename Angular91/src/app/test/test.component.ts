2be72ae1467c0c3a3337e695ddb5cfe58d857008
pamuba <partha.bora24@gmail.com>
2022-01-22 01:32:26 +0530

    544

--- Angular91/src/app/test/test.component.ts
+++ Angular91/src/app/test/test.component.ts
@@ -2,13 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'testComp',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+     <h2>Welcome to the TEST App</h2>
+     <h4>Property Binding</h4>
+     <input [id]="txtName" type="text" value="property binding" /><br>
+     <input [disabled]="false" id="{{txtName}}" type="text" value="property binding" /><br>
+     <input [disabled]="isDisabled" id="{{txtName}}" type="text" value="property binding" /><br>
+     <button>Click</button>
+  `,
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   constructor() { }
-
+  public txtName = "prop"
+  public isDisabled = false
   ngOnInit(): void {
   }
 
