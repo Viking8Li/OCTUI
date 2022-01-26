3af961b85cd22fe01331be2b2588d840f684620e
pamuba <partha.bora24@gmail.com>
2022-01-26 20:11:16 +0530

    5611

--- AngularAuth/ngApp/src/app/app.module.ts
+++ AngularAuth/ngApp/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { EventService } from './event.service';
+import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,13 @@ import { EventService } from './event.service';
     FormsModule,
     HttpClientModule
   ],
-  providers: [AuthService, EventService],
+  providers: [AuthService, AuthGuard, EventService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
