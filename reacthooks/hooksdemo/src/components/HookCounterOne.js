5d70690ec06b2a3acd84194a63095e88accb1087
pamuba <partha.bora24@gmail.com>
2022-02-07 22:07:46 +0530

    639

--- reacthooks/hooksdemo/src/components/HookCounterOne.js
+++ reacthooks/hooksdemo/src/components/HookCounterOne.js
@@ -3,13 +3,16 @@ import React, {useState, useEffect} from 'react';
 function HookCounterOne() {
 
  const [count, setCount] = useState(0)
+ const [name, setName] = useState('')
 
  //componentDidMount + componentDidUpdate
  useEffect(()=>{
+     console.log('useEffect-Updating the document title');
      document.title =  `You clicked ${count} times`
- })
+ }, [count])
 
   return <>
+      <input type="text" value={name} onChange={e => setName(e.target.value)}></input>
       <button onClick={()=>setCount(count + 1)}>Click {count} times</button>
   </>;
 }
