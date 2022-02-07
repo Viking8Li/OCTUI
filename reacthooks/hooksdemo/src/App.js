4815a1c44d24fb296b253fdcd2fd8ec6f90bb7ea
pamuba <partha.bora24@gmail.com>
2022-02-07 21:26:34 +0530

    634

--- reacthooks/hooksdemo/src/App.js
+++ reacthooks/hooksdemo/src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import HookCounter from './components/HookCounter';
 import ClassCounter from './components/ClassCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <ClassCounter></ClassCounter> 
-       <HookCounter></HookCounter>
+       {/* <ClassCounter></ClassCounter> 
+       <HookCounter></HookCounter> */}
+
+       <HookCounterTwo></HookCounterTwo>
+
       </header>
     </div>
   );
