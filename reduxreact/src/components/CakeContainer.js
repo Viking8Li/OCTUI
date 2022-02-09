f1594737f9b32014597f8173e2153969bff7f852
pamuba <partha.bora24@gmail.com>
2022-02-09 20:37:15 +0530

    6511

---
+++ reduxreact/src/components/CakeContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import { buyCake } from './redux/cakes/cakeActions';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
+
+function CakeContainer(props) {
+  return <>
+    <h2>Number Of Cakes - {props.numOfCakes}</h2>
+    <button onClick={props.buyCake}>Buy Cake</button>
+  </>;
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+} 
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
---
+++ reduxreact/src/redux/cakes/cakeActions.js
@@ -0,0 +1,7 @@
+import { BUY_CAKE } from './cakeTypes'
+//Actions
+export const buyCake = () => {
+    return {
+        type: BUY_CAKE
+    }
+}
