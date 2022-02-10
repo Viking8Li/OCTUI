9b57049966585bf163f0a5b4a09ff09a3cd7bacc
pamuba <partha.bora24@gmail.com>
2022-02-11 03:29:09 +0530

    77733

--- reacttest/src/components/Autocomplete.jsx
+++ reacttest/src/components/Autocomplete.jsx
@@ -18,12 +18,12 @@ function Autocomplete() {
         suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
     );
 
-    this.setState({
-      activeSuggestion: 0,
-      filteredSuggestions,
-      showSuggestions: true,
-      userInput: e.currentTarget.value
-    });
+   
+      setActiveSuggestion(0)
+      setFilteredSuggestions(setFilteredSuggestions)
+      setShowSuggestions(true)
+      setUserInput(e.currentTarget.value)
+    
   };
 
   const onClick = e => {
@@ -33,6 +33,28 @@ function Autocomplete() {
       showSuggestions: false,
       userInput: e.currentTarget.innerText
     });
+  };
+
+  const  onKeyDown = e => {
+    if (e.keyCode === 13) {
+      this.setState({
+        activeSuggestion: 0,
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion]
+      });
+    } else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) {
+        return;
+      }
+      this.setState({ activeSuggestion: activeSuggestion - 1 });
+    }
+    // User pressed the down arrow, increment the index
+    else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) {
+        return;
+      }
+      this.setState({ activeSuggestion: activeSuggestion + 1 });
+    }
   };
 
   const getData = () => {
@@ -53,16 +75,49 @@ function Autocomplete() {
   },[]);
 
 
+  let suggestionsListComponent;
+
+    if (showSuggestions && userInput) {
+      if (filteredSuggestions.length) {
+        suggestionsListComponent = (
+          <ul className="suggestions">
+            {filteredSuggestions.map((suggestion, index) => {
+              let className;
+
+              // Flag the active suggestion with a class
+              if (index === activeSuggestion) {
+                className = "suggestion-active";
+              }
+
+              return (
+                <li className={className} key={suggestion} onClick={onClick}>
+                  {suggestion}
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        suggestionsListComponent = (
+          <div className="no-suggestions">
+            <em>No suggestions, you're on your own!</em>
+          </div>
+        );
+      }
+    }
+  
   return (
       
     <div className="wrapper">
-      <h5>Try typing these words</h5>
-      <br />
-      <input
+        
+        <input
           type="text"
           onChange={onChange}
+          onKeyDown={onKeyDown}
           value={userInput}
         />
+        {suggestionsListComponent}
+     
     </div>
   )
 }
